18011aacaa79ddae42a6148e0501372517cb4e7a
McKenzie <mckenzid@oxy.com>
2016-10-13 07:51:41 -0500

    Added the base component files

---
+++ app/grids/grids-sample.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'oxy-grid',
+    templateUrl: 'app/grids/grids-sample.component.html'
+})
+export class GridsSampleComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
