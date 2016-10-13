03f6c1a65d3d80340bcfc8f7d42765228cef7104
McKenzie <mckenzid@oxy.com>
2016-10-13 06:41:09 -0500

    Added grid package. Minor text change

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular App And I made a change</h1><h2>And it is auto-updating</h2>'
+    template: '<h1>My First Angular App And I made a change</h1><h2>And it is auto-updating</h2><h2>This has the Grid</h2>'
 })
 export class AppComponent { }
